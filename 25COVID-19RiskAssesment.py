f4d5d3706ee41b2920e84e0330244ee3bf3d9f8d
flovearth <flovearth@protonmail.com>
2020-07-07 18:14:16 +0200

    This python scripts turns out your affection risk of COVID-19. Give a try.

---
+++ 25COVID-19RiskAssesment.py
@@ -0,0 +1,12 @@
+age = input('Are you a cigarette addict older than 75 years old? (yes/no) ')
+age = age.lower()  # YES or Yes or yes doesnt matter.
+chronic = input('Do you have a severe chronic disease?(yes/no) ')
+chronic = chronic.lower()
+immune = input('Is your immune system too weak?(yes/no) ')
+immune = immune.lower()
+risk = bool(age=="yes" and chronic=="yes"  and immune=="yes" )
+
+if risk:
+    print('You are in risky group.')
+else:
+    print('You are not in risky group.')
